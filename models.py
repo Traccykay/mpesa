36a1df3153946f2a3aafae0c946dd310b1089273
Traccykay <traccykay@gmail.com>
2017-04-04 10:28:03 +0300

    Added a drop down selection for deposit and withdraw

--- models.py
+++ models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Transaction(models.Model):
+	DEPOSIT = 'DP'
+	WITHDRAW = 'WD'
+	Type_of_transactions = (
+ 		(DEPOSIT, 'Deposit'),
+ 		(WITHDRAW, 'Withdraw'),
+ 		)
+
+
 	amount = models.IntegerField()
 	date_of_transaction = models.DateTimeField()
-	transaction_type = models.CharField(max_length=20)
+	transaction_type = models.CharField(max_length=20, choices=Type_of_transactions, default=DEPOSIT,)
 	cost = models.IntegerField()
 	phone_number = models.CharField(max_length=20)
